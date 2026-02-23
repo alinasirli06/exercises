3aada52674033cc0811bd473d009a6f281390985
alinasirli06 <nesirovelihuseyn06@gmail.com>
2026-02-23 17:47:57 +0400

    task 17 added

---
+++ task 17/app.js
@@ -0,0 +1,12 @@
+// 1
+// console.log("Hello world");
+
+// 2
+// const myName = "Ali";
+// console.log(myName);
+
+// 4
+// const userName = prompt ("Adınızı daxil edin:");
+// const surname = prompt ("Soyadınızı daxil edin:");
+// console.log("Ad:",userName);
+// console.log("Soyad:",surname);  
